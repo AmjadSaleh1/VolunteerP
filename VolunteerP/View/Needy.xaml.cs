76e1ddc5b2db4946b74c51f047fc7b8f34a4f1a4
amjad <amjad@DESKTOP-9M61TLV>
2024-04-04 17:01:56 +0300

    Add project files.

---
+++ VolunteerP/View/Needy.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VolunteerP.View
+{
+    /// <summary>
+    /// Interaction logic for Needy.xaml
+    /// </summary>
+    public partial class Needy : Window
+    {
+        public Needy()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files|*.bmp;*.jpg;*png";
+            openDialog.FilterIndex = 1;
+            if (openDialog.ShowDialog() == true)
+            {
+                var url = openDialog.FileName;
+                this.UserProfileImage.ImageSource = new BitmapImage(new Uri(url));
+            }
+        }
+    }
+}
---
+++ VolunteerP/ViewModel/NavigationVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VolunteerP.Utilities;
+using System.Windows.Input;
+using System.Transactions;
+using VolunteerP.View;
+
+namespace VolunteerP.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand HomeCommand { get; set; }
+        public ICommand ProductsCommand { get; set; }
+        public ICommand ProfileCommand { get; set; }  
+  
+     
+
+        private void Home(object obj) => CurrentView = new HomeVm();
+        private void Product(object obj) => CurrentView = new ProductVm();
+        private void Profile(object obj) => CurrentView = new ProfileVm();
+
+
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            ProductsCommand = new RelayCommand(Product);
+            ProfileCommand = new RelayCommand(Profile);
+
+            // Startup Page
+            CurrentView = new HomeVm();
+        }
+    }
+}
