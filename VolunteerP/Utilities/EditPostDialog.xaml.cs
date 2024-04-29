4c45f3024ee4a3015d53f2afb46e3a54efdc563d
amjadsalih1809@gmail.com <amjadsalih1809@gmail.com>
2024-04-29 14:30:51 +0300

    photo editing /removing updated

--- VolunteerP/Utilities/EditPostDialog.xaml.cs
+++ VolunteerP/Utilities/EditPostDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,18 +31,53 @@ namespace VolunteerP.Utilities
             set { PostContentTextBox.Text = value; }
         }
 
+        public string ImagePath
+        {
+            get { return _post.ImagePath; }
+            set { _post.ImagePath = value; UpdateImageDisplay(); }
+        }
+
         public EditPostDialog(Post post, PostService postService)
         {
             InitializeComponent();
             _postService = postService;
             _post = post;
             PostContentTextBox.Text = post.UserPost;
+            PostContentTextBox.Text = post.UserPost;
+            UpdateImageDisplay();
         }
+
+        private void UpdateImageDisplay()
+        {
+            PostImage.Source = string.IsNullOrEmpty(_post.ImagePath) ? null : new BitmapImage(new Uri(_post.ImagePath));
+        }
+
+        private void ChangeImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp",
+                Title = "Select a Photo"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImagePath = openFileDialog.FileName;
+            }
+        }
+
+        private void RemoveImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImagePath = null; // Clears the image path
+        }
+
+
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             _post.UserPost = PostContentTextBox.Text;
-            _postService.UpdatePostAsync(_post.Id, _post.UserPost);
+            _postService.UpdatePostAsync(_post.Id, _post.UserPost); // Update text
+            _postService.UpdatePostImagePath(_post.Id, _post.ImagePath);  // This should handle `null` as a "delete" operation
             this.DialogResult = true;
         }
     }
